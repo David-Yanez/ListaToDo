132eb3efd9415f9e10affdf22286abba02ff6b66
David-Yanez <luis.yanez@epn.edu.ec>
2023-11-22 20:44:46 -0500

    implementación buscar todo

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ import { CreateTodoButton } from "./CreateTodoButton"
 import './App.css'
 
 const defaultTodos = [
-  {text: 'Cortar cebolla', completed:true},
+  {text: 'Cortar cebolla', completed:false},
   {text: 'Cortar carne', completed:true},
   {text: 'Tender la cama', completed:false},
-  {text: 'Lavar platos', completed:false},
+  {text: 'Lavar platos', completed:true},
   {text: 'Cocinar el almuerzo', completed:true},
 ]
 
@@ -20,9 +20,11 @@ function App() {
   const [todos, setTodos] = useState(defaultTodos)
   const [searchValue, setSearchValue] =React.useState("");
 // !! convierte cualquier dato a true o false (true = cualqueir valor diferente de 0, -1, null, undefine, etc)
-  const completedTodos = todos.filter(todo => todo.completed).length
+  const completedTodos = todos.filter(todo => !!todo.completed).length
   console.log(completedTodos)
   const totalTodos = todos.length
+
+  const searchedTodos = todos.filter(todo => todo.text.toLowerCase().includes(searchValue.toLowerCase()))
 
   return (
     <>
@@ -33,7 +35,7 @@ function App() {
       setSearchValue={setSearchValue}/>
 
       <TodoList>
-        {defaultTodos.map(todo => 
+        {searchedTodos.map(todo => 
         <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
         )}
       </TodoList>
