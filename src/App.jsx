9ff2d2980c74b9c22bd74e19c0bef31f8b8a21ac
David-Yanez <luis.yanez@epn.edu.ec>
2023-11-29 23:38:49 -0500

    organización componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
-import { TodoCounter } from "./TodoCounter"
-import { TodoSearch } from "./TodoSearch"
-import { TodoList } from "./TodoList"
-import { TodoItem } from "./TodoItem"
-import { CreateTodoButton } from "./CreateTodoButton"
+import { TodoCounter } from "./TodoCounter/TodoCounter"
+import { TodoSearch } from "./TodoSearch/TodoSearch"
+import { TodoList } from "./TodoList/TodoList"
+import { TodoItem } from "./TodoItem/TodoItem"
+import { CreateTodoButton } from "./CreateTodoButton/CreateTodoButton"
 import './App.css'
+import { useLocalStorage } from './Hooks/useLocalStorege'
 
 const defaultTodos = [
   {text: 'Cortar cebolla', completed:false},
@@ -18,29 +19,6 @@ const defaultTodos = [
 localStorage.setItem('TODOS_V1', StringifiedTodos)
 localStorage.removeItem('TODOS_V1')*/
 
-function useLocalStorage(itemName, initialValue) {
-
-  
-
-  const localStorageItem = localStorage.getItem(itemName)
- 
-  let parsedItem
-  if (!localStorageItem){
-    localStorage.setItem(itemName, JSON.stringify(initialValue))
-    parsedItem = initialValue
-  } else {
-    parsedItem = JSON.parse(localStorageItem)
-  }
-
-  const [item, setItem] = useState(parsedItem)
-
-  const saveItem = (newItem) =>{
-    localStorage.setItem(itemName, JSON.stringify(newItem))
-    setItem(newItem)
-  }
-
-  return [item, saveItem]
-}
 
 function App() {
  
@@ -53,9 +31,6 @@ function App() {
   const totalTodos = todos.length
 
   const searchedTodos = todos.filter(todo => todo.text.toLowerCase().includes(searchValue.toLowerCase()))
-
-
-  
 
 
   const completeTodo = (text) =>{
--- src/TodoCounter/TodoCounter.jsx
+++ src/TodoCounter/TodoCounter.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './TodoCouter.css'
+import './TodoCounter.css'
 
 export const TodoCounter = ({total, completed}) => {
   return (
